7542557a7b5aeb2670430fdef0641545555dbb9c
Marcelo Alonso <mclonso123@outlook.com>
2024-09-16 20:52:44 -0300

    3

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     python_requires='>=3.11',
     entry_points={
         'console_scripts': [
-            f'camera={MODULE_STUB}.main:main',
+            f'camera={MODULE_STUB}.app.run',
         ],
     },
     install_requires=[  # Instala as dependências especificadas no requirements.txt
