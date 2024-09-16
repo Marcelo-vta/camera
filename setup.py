cd6967b7afddd645e9bd87dd65050cddb5ee9ace
Marcelo Alonso <125833252+Marcelo-vta@users.noreply.github.com>
2024-09-16 20:14:02 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def find_subdir(start_dir):
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-MODULE_STUB = "camera-irl"
+MODULE_STUB = "camera"
 
 setup(
     name=MODULE_STUB,  # Substitua pelo nome do seu pacote
